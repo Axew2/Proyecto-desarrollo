19600989c2a70dca210f32ab4f445989aa3ff175
basti4ncistern4as <bastian.cisternas1@mail.udp.cl>
2026-09-12 10:59:36 -0300

    ligeros cambios en el login.js porque no funcionban algunas cosas con los cambios actuales y mejora en el boton contraseña dentro del styles y en login

--- login.js
+++ login.js
@@ -2,6 +2,7 @@ const contrasenia = document.querySelector("#password");
 const botonMostrar = document.createElement("button");
 
 botonMostrar.type = "button";
+botonMostrar.className = "Boton-mostrar";
 botonMostrar.textContent = "Mostrar Contraseña";
 
 contrasenia.after(botonMostrar);
