f43e000e43217bc6c0189be4556fc18b42d59ae9
basti4ncistern4as <bastian.cisternas1@mail.udp.cl>
2026-09-12 10:26:41 -0300

    Algunas correcciones del estado biblioteca y se agregaron escuchados tanto a la cancion como las demas

--- biblioteca.js
+++ biblioteca.js
@@ -11,3 +11,26 @@ botonSola.addEventListener("click", function(){
 
 });
 
+const botonFromTheStart = document.querySelector("#boton-from-the-start");
+
+botonFromTheStart.addEventListener("click", function(){
+
+    if (botonFromTheStart.textContent === "Marcar como escuchada"){
+        botonFromTheStart.textContent = "Escuchada";
+    }
+    else{
+        botonFromTheStart.textContent = "Marcar como escuchada";
+    }
+
+});
+
+const botonManiac = document.querySelector("#boton-maniac");
+
+botonManiac.addEventListener("click", function(){
+    if(botonManiac.textContent === "Marcar como escuchada"){
+        botonManiac.textContent = "Escuchada";
+    }
+    else{
+        botonManiac.textContent = "Marcar como escuchada";
+    }
+});
